0d18a9910986a0f140e51680268b8e8cf1769f9b
Bilal Mubarak <mbilal3241@gmail.com>
2023-01-30 21:14:01 +0500

    videos added

--- components/Demo.tsx
+++ components/Demo.tsx
@@ -5,29 +5,41 @@ const Demo = () => {
         <h2 className="text-5xl font-bold sm:text-4xl">Demo Video</h2>
 
         <p className="mt-4 text-2xl sm:text-xl">
-          At nibh volutpat sollicitudin consequat viverra accumsan nunc. Quam dolor vestibulum tempus risus sagittis amet.
+          At nibh volutpat sollicitudin consequat viverra accumsan nunc. Quam
+          dolor vestibulum tempus risus sagittis amet.
         </p>
       </div>
-      <div className="pt-12">
-        <img className="video-img" src="/images/demo_images.svg"/>
+      <div className="pt-12 relative">
+        <img className="video-img" src="/images/demo_images.svg" />
+        <video
+          controls
+          className="absolute left-[381px] top-[90px] w-[614px] h-[370px]"
+        >
+          <source src="IMG_4956.mp4" type="video/mp4" />
+        </video>
       </div>
       <div className="mx-auto max-w-3xl pt-5 text-center">
-        <h2 className="text-5xl font-bold sm:text-4xl">Cursus sodales tincidunt tempus sed sagittis blandit</h2>
+        <h2 className="text-5xl font-bold sm:text-4xl">
+          Cursus sodales tincidunt tempus sed sagittis blandit
+        </h2>
 
         <p className="mt-4 opacity-50 leading-7">
-          Lorem sagittis feugiat est rhoncus sed. Lacus in tincidunt bibendum nisl. Porttitor consequat pulvinar nulla adipiscing orci ac mattis. Neque commodo pharetra morbi nisi ut duis purus dictum. Mauris amet facilisis elementum vestibulum.
+          Lorem sagittis feugiat est rhoncus sed. Lacus in tincidunt bibendum
+          nisl. Porttitor consequat pulvinar nulla adipiscing orci ac mattis.
+          Neque commodo pharetra morbi nisi ut duis purus dictum. Mauris amet
+          facilisis elementum vestibulum.
         </p>
       </div>
-      <div  className="mx-auto max-w-sm pt-11 flex justify-between">
+      <div className="mx-auto max-w-sm pt-11 flex justify-between">
         <button>
-          <img className="bg-white" src="/images/google_play_btn.svg"/>
+          <img className="bg-white" src="/images/google_play_btn.svg" />
         </button>
         <button>
-          <img className="bg-white" src="/images/app_store_btn.svg"/>
+          <img className="bg-white" src="/images/app_store_btn.svg" />
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
--- components/UnSplash.tsx
+++ components/UnSplash.tsx
@@ -6,21 +6,29 @@ const UnSplash = () => {
           <div>
             <div>
               <h2 className="text-5xl font-bold">Our Mission</h2>
-              <h4 className="text-4xl leading-10 font-medium pt-7">Nec amet libero morbi nunc 
-              <br/> posu ere dis sit </h4>
+              <h4 className="text-4xl leading-10 font-medium pt-7">
+                Nec amet libero morbi nunc
+                <br /> posu ere dis sit{" "}
+              </h4>
             </div>
             <div className="flex items-center max-w-lg justify-between pt-8">
               <div>
                 <p className="leading-10">Why work with us?</p>
                 <ul className="mission">
-                  <li className="leading-7">Ornare velit non amet id risus sit <br/>curabitur dignissim.</li>
-                  <li >Mus lobortis risus mollis eu.</li>
+                  <li className="leading-7">
+                    Ornare velit non amet id risus sit <br />
+                    curabitur dignissim.
+                  </li>
+                  <li>Mus lobortis risus mollis eu.</li>
                 </ul>
               </div>
               <div>
                 <p className="leading-10">Mus lobortis risus mollis eu.</p>
                 <ul className="mission">
-                  <li className="leading-7">Eget duis lobortis sagittis ligula <br/>mauris habitant.</li>
+                  <li className="leading-7">
+                    Eget duis lobortis sagittis ligula <br />
+                    mauris habitant.
+                  </li>
                   <li>Morbi sem erat sed luctus nulla.</li>
                 </ul>
               </div>
@@ -32,12 +40,18 @@ const UnSplash = () => {
             </button>
           </div>
         </div>
-        <div className="video_bg1 mt-4 pt-36 h-2/3">
-          <img className="w-full" src="images/video_bg1.svg"/>
+        <div className="video_bg1 relative mt-4 pt-36 h-2/3">
+          <img className="w-full" src="images/video_bg1.svg" />
+          <video
+            controls
+            className="absolute left-[113px] top-[170px] w-[810px] h-[460px]"
+          >
+            <source src="IMG_4880.mp4" type="video/mp4" />
+          </video>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default UnSplash
+export default UnSplash;
